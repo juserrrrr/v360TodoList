d2e4582fc8b765cd4537455f964cfc5bf875320f
José Gabriel <gabriel-pontes09@hotmail.com>
2024-12-24 11:53:07 -0300

    task: implement lists management with CRUD functionality

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,70 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[ show edit update destroy ]
+
+  # GET /lists or /lists.json
+  def index
+    @lists = List.all
+  end
+
+  # GET /lists/1 or /lists/1.json
+  def show
+  end
+
+  # GET /lists/new
+  def new
+    @list = List.new
+  end
+
+  # GET /lists/1/edit
+  def edit
+  end
+
+  # POST /lists or /lists.json
+  def create
+    @list = List.new(list_params)
+
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to @list, notice: "List was successfully created." }
+        format.json { render :show, status: :created, location: @list }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /lists/1 or /lists/1.json
+  def update
+    respond_to do |format|
+      if @list.update(list_params)
+        format.html { redirect_to @list, notice: "List was successfully updated." }
+        format.json { render :show, status: :ok, location: @list }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /lists/1 or /lists/1.json
+  def destroy
+    @list.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to lists_path, status: :see_other, notice: "List was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Uso de callbacks para compartir código entre ações
+    def set_list
+      @list = List.find(params.expect(:id))
+    end
+
+    # Apenas permitir uma lista de parâmetros confiáveis através de strong parameters
+    def list_params
+      params.expect(list: [ :name ])
+    end
+end
