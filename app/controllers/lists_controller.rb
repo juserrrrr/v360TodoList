efea89262fa1f24c2e447efa0afe4bd1f65c9711
José Gabriel <gabriel-pontes09@hotmail.com>
2024-12-27 17:34:47 -0300

    task: implement user logged-in layout and update titles for better user experience

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,7 @@
 class ListsController < ApplicationController
   allow_unauthenticated_access
   before_action :set_list, only: %i[ show edit update destroy ]
+  layout 'user_logged_in'
 
   # GET /lists or /lists.json
   def index
