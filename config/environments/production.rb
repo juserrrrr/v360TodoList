3f22544fa23ae2a7beb2b355c7a95fcf3733833d
José Gabriel <gabriel-pontes09@hotmail.com>
2024-12-28 01:10:14 -0300

    task: update production environment configuration to disable SSL and add Docker Compose setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,13 +25,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = true
-
-  config.action_dispatch.trusted_proxies = ['0.0.0.0/0', '::/0'] # Ou o IP do seu proxy reverso (Traefik ou Caddy)
-  config.action_dispatch.x_forwarded_proto = true
+  config.assume_ssl = false
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
